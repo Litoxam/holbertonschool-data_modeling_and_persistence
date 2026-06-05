56ca3b2634028f680b54ee5d32d6b90517800150
Litoxam <litobernier@gmail.com>
2026-06-05 16:44:37 +0200

    fix: GROUP BY courses.id

--- sql_join/7-course-enrollment-count.sql
+++ sql_join/7-course-enrollment-count.sql
@@ -3,5 +3,5 @@ COUNT(enrollments.student_id) AS enrollment_count
 FROM courses
 LEFT JOIN enrollments
 ON courses.id = enrollments.course_id
-GROUP BY course_id
+GROUP BY courses.id
 ORDER BY enrollment_count DESC, course_title ASC;
