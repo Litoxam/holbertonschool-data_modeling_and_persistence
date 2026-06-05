ea70371d9b4e12378fea55646cebcf7d00439806
Litoxam <litobernier@gmail.com>
2026-06-05 12:48:15 +0200

    fix: use of aliases and INNER JOIN

--- sql_join/1-books-with-authors.sql
+++ sql_join/1-books-with-authors.sql
@@ -1,5 +1,5 @@
-SELECT books.title, authors.name
+SELECT books.title AS title, authors.name AS author_name
 FROM books
-JOIN authors
+INNER JOIN authors
 ON books.author_id = authors.id
 ORDER BY title ASC;
